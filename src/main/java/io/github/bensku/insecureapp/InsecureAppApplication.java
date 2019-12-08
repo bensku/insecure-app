80b581a474579a3830d39dd739e0c35ee199be5b
bensku <bmi@iki.fi>
2019-12-08 20:10:51 +0200

    Initialize

---
+++ src/main/java/io/github/bensku/insecureapp/InsecureAppApplication.java
@@ -0,0 +1,13 @@
+package io.github.bensku.insecureapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsecureAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsecureAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/bensku/insecureapp/InsecureAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bensku.insecureapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsecureAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
